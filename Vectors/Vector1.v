2d8a66c1904ca357181b33cf42c92bb2396b80e3
Jimmyyuuuu <jimmy26620363@gmail.com>
2023-06-21 20:41:26 +0800

    vector1 法2

--- Vectors/Vector1.v
+++ Vectors/Vector1.v
@@ -8,3 +8,17 @@ module top_module(
     
 
 endmodule
+// 法2
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
+module top_module( 
+    input wire [15:0] in,
+    output wire [7:0] out_hi,
+    output wire [7:0] out_lo );
+    assign {out_hi, out_lo} = in;
+    
+    
+
+endmodule
+
+
+
