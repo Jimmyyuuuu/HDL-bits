d1fe33fd17c80e2b6d92d8b5a1673b8847c7c48f
Jimmyyuuuu <jimmy26620363@gmail.com>
2023-06-26 12:26:42 +0800

    reversal

--- Vectors/Vector6-reversal.v
+++ Vectors/Vector6-reversal.v
@@ -5,3 +5,10 @@ module top_module(
     assign out={in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
 
 endmodule
+
+// 別種方式
+// loop 
+// always @(*) begin
+// for(int i=0; i<8;i++)
+// out[i]=in[8-i-1];
+// end
