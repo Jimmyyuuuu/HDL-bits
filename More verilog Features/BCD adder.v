9a06e91ce4d5c7408dc836a6a812136bbefaeb5b
Jimmyyuuuu <jimmy26620363@gmail.com>
2023-07-04 22:40:48 +0800

    adder

--- More verilog Features/BCD adder.v
+++ More verilog Features/BCD adder.v
@@ -38,6 +38,3 @@ module top_module(
         assign cout = cout_temp[99];
     endgenerate
 endmodule
-————————————————
-版权声明：本文为CSDN博主「☆柒⑦☆」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/wszwszwszqwer/article/details/123764784
