bda87a9576b00f95fa8fb62cfbad78ee1fa21caa
Jimmyyuuuu <jimmy26620363@gmail.com>
2023-08-04 13:47:23 +0800

    非同步正緣reset

--- Sequential Circuits- Shift Registers/shift4.v
+++ Sequential Circuits- Shift Registers/shift4.v
@@ -18,3 +18,27 @@ module top_module(
     end
 
 endmodule
+//法2
+module top_module(
+    input clk,
+    input areset,  // async active-high reset to zero
+    input load,
+    input ena,
+    input [3:0] data,
+    output reg [3:0] q); 
+// Asynchronous reset: Notice the sensitivity list.
+	// The shift register has four modes:
+	//   reset
+	//   load
+	//   enable shift
+	//   idle -- preserve q (i.e., DFFs)
+	always @(posedge clk, posedge areset) begin
+		if (areset)		// reset
+			q <= 0;
+		else if (load)	// load
+			q <= data;
+		else if (ena)	// shift is enabled
+			q <= q[3:1];	// Use vector part select to express a shift.
+	end
+	
+endmodule
